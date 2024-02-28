1c7c3a33e776f9bf5261e1565c2c0d280adffe7f
JonhBusiness <jonhbusiness2022@gmail.com>
2024-02-28 15:57:35 -0600

    cambios

--- visual.js
+++ visual.js
@@ -1,25 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Selecciona el checkbox
   var checkbox = document.getElementById("fondo");
-  var caja = document.querySelector(".cajatexto");
-  var cajatexto = document.querySelector(".contenedorcajatexto");
+  // var caja = document.querySelector(".cajatexto");
+  // var cajatexto = document.querySelector(".contenedorcajatexto");
   var p = document.querySelector(".alerta p");
   var text = document.querySelector(".contenedorcajatexto textarea");
 
   // Cambia el color de fondo del body cuando el estado del checkbox cambia
   checkbox.addEventListener("change", function () {
     if (checkbox.checked) {
-      caja.style.color = "#0a3871";
+      // caja.style.color = "#0a3871";
       document.body.style.background = "linear-gradient(to bottom, #d4ddef, #c6d3ef, #b7c9ef, #a9c0ee, #9ab6ee)";
-      cajatexto.style.background = "none";
+      // cajatexto.style.background = "none";
       p.style.color = "#495057";
-      text.className = "textarea";
+      // text.className = "textarea";
       text.className = "cajatexto";
+      text.style.background = "transparent";
     } else {
       document.body.style.background = "linear-gradient(to bottom, #3359a3, #25467f, #1a335d, #11213d, #070f1f)";
-      cajatexto.style.background = "white";
+      text.style.background = "white";
       p.style.color = "#9ab6ee";
-      text.className = "estilo";
+      // text.className = "estilo";
+      text.classList.add("estilo");
     }
   });
 });
